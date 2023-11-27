a4be7808865270c7db967b3475f111df2bfc3df3
TrujiDev <eetgrisales@gmail.com>
2023-11-27 15:33:32 -0500

    Add increment and decrement functionality to quantity input

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Header from './components/Header';
+import Button from './components/Button';
 
 function App() {
 	const [quantity, setQuantity] = useState(10000);
@@ -12,9 +13,36 @@ function App() {
 		setQuantity(+evt.target.value);
 	}
 
+	function handleDecrement() {
+		const value = quantity - STEP;
+
+		if (value < MIN) {
+			alert('Cantidad mínima alcanzada');
+			return;
+		}
+
+		setQuantity(value);
+	}
+
+	function handleIncrement() {
+		const value = quantity + STEP;
+
+		if (value > MAX) {
+			alert('Cantidad máxima alcanzada');
+			return;
+		}
+
+		setQuantity(value);
+	}
+
 	return (
 		<div className='my-5 max-w-lg mx-auto bg-white shadow p-10'>
 			<Header />
+
+			<div className='flex justify-between my-6'>
+				<Button operator='-' fn={handleDecrement} />
+				<Button operator='+' fn={handleIncrement} />
+			</div>
 
 			<input
 				className='w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600'
