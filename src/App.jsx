5b0ce151e5741a2748a87df2acfc73ce0befad62
TrujiDev <eetgrisales@gmail.com>
2023-11-27 14:46:15 -0500

    Add Header component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
-	return <></>;
+	return (
+		<div className='my-20 max-w-lg mx-auto bg-white shadow p-10'>
+			<Header />
+		</div>
+	);
 }
 
 export default App;
