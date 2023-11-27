93227ab26c5ea5953a10b7c8aca182f3e2bc508c
TrujiDev <eetgrisales@gmail.com>
2023-11-27 15:10:22 -0500

    Update quantity range in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ import Header from './components/Header';
 function App() {
 	const [quantity, setQuantity] = useState(10000);
 
+	const MIN = 0;
+	const MAX = 20000;
+	const STEP = 100;
+
 	function handleChange(evt) {
 		setQuantity(+evt.target.value);
 	}
 
 	return (
-		<div className='my-20 max-w-lg mx-auto bg-white shadow p-10'>
+		<div className='my-5 max-w-lg mx-auto bg-white shadow p-10'>
 			<Header />
 
 			<input
 				className='w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600'
 				type='range'
 				onChange={handleChange}
+				min={MIN}
+				max={MAX}
+				step={STEP}
+				value={quantity}
 			/>
 
 			<p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>
