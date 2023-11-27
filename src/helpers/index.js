e46f664b7801fad3be2f944ef4a6057e36ebb8f2
TrujiDev <eetgrisales@gmail.com>
2023-11-27 17:08:43 -0500

    Refactor formatMoney function and add calculateTotalPay function

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,8 +1,33 @@
 function formatMoney(value) {
-    return value.toLocaleString("en-US", {
-        style: "currency",
-        currency: "USD",
-    });
+	return value.toLocaleString('en-US', {
+		style: 'currency',
+		currency: 'USD',
+	});
 }
 
-export default formatMoney;
+const calculateTotalPay = (quantity, deadline) => {
+    let total;
+
+    if (quantity < 5000) {
+        total = quantity * 1.5;
+    } else if (quantity >= 5000 && quantity < 10000) {
+        total = quantity * 1.4;
+    } else if (quantity >= 10000 && quantity < 15000) {
+        total = quantity * 1.3;
+    } else {
+        total = quantity * 1.2;
+    }
+
+    // Calculate deadline
+    if (deadline === 6) {
+        total *= 1.1;
+    } else if (deadline === 12) {
+        total *= 1.2;
+    } else if (deadline === 24) {
+        total *= 1.3;
+    }
+
+    return total;
+};
+
+export { formatMoney, calculateTotalPay };
