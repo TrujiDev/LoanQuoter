f15cd7308c9ec372cab397c455a98f946f9f0ad3
TrujiDev <eetgrisales@gmail.com>
2023-11-27 15:06:14 -0500

    Add quantity state and handleChange function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import Header from './components/Header';
 
 function App() {
+	const [quantity, setQuantity] = useState(10000);
+
+	function handleChange(evt) {
+		setQuantity(+evt.target.value);
+	}
+
 	return (
 		<div className='my-20 max-w-lg mx-auto bg-white shadow p-10'>
 			<Header />
@@ -8,8 +15,12 @@ function App() {
 			<input
 				className='w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600'
 				type='range'
-				
+				onChange={handleChange}
 			/>
+
+			<p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>
+				{quantity}
+			</p>
 		</div>
 	);
 }
